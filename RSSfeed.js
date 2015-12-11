9944c66eb754f9043d2f1e8f2194496d64d74d80
Erika Villegas <evillegas@student.neumont.edu>
2015-12-11 10:39:14 -0700

    Rss feed for kpop

--- RSSfeed.js
+++ RSSfeed.js
@@ -1,15 +1,19 @@
+
+
 $.ajax({
-  url      : document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent('http://www.korea.net/koreanet/rss/newsfocus/culture'),
+  url      : document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent('http://world.kbs.co.kr/rss/rss_enternews.htm?lang=e'),
   dataType : 'json',
   success  : function (data) {
     if (data.responseData.feed && data.responseData.feed.entries) {
+         var container = $('<kpop />');
       $.each(data.responseData.feed.entries, function (i, e) {
-        console.log("------------------------");
+          $(function () {  
+    $('<p>' + e.title +'</p>').appendTo('#kpop');
+     $('<p>'+e.content + '</p>').appendTo('#kpop');
+});
         console.log("title      : " + e.title);
-        console.log("author     : " + e.author);
         console.log("content: " + e.content);
       });
     }
   }
-   
 });
