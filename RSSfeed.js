6c342a05fbcb0d7ae9537d7eea10e999c17cbcf6
Erika Villegas <evillegas@student.neumont.edu>
2015-12-15 00:13:09 -0700

    Backgrounds done, date and time as well #wearealmostdoneeeee

--- RSSfeed.js
+++ RSSfeed.js
@@ -1,3 +1,76 @@
+backgroundColor();
+function backgroundColor(){    
+    var currentTime = new Date().getHours();
+    if(7 <= currentTime && currentTime < 20){
+        document.getElementById('clearsky').style.backgroundColor = "#87CEFA";
+    }else{
+       document.getElementById('clearsky').style.backgroundColor = "#000217";
+    }
+}
+
+ var today = new Date();
+
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+var week = new Array(7);
+
+dates();
+
+function dates(){
+    
+    var day = today.getDate();
+    var mo = today.getMonth();
+    var year = today.getFullYear();
+    
+    week[0] = "Sunday";
+    week[1] = "Monday";
+    week[2] = "Tuesday";
+    week[3] = "Wednesday";
+    week[4] = "Thursday";
+    week[5] = "Friday";
+    week[6] = "Saturday";
+    
+    var dw = week[today.getDay()];
+    
+    
+    document.getElementById('date').innerHTML = dw +" "+ mo + "/" + day + "/" + year;
+}
+ 
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    var time = " ";
+    m = checkTime(m);
+    s = checkTime(s);
+    if(h > 11){
+        time = "p.m."
+    }if(h <11){
+        time = "a.m."
+    }
+    
+    if(h > 12){
+       h = h - 12;
+    }
+    if(h == 0){
+        h = 12;
+    }
+    
+    
+    
+    document.getElementById('time').innerHTML = h + ":" + m + ":" + s + " " + time;
+    t = setTimeout(function () {
+        startTime()
+    }, 500);
+}
+startTime();
+
 $(document).ready(function () {
     getArticles('http://www.npr.org/rss/rss.php?id=1001', '#bus');
     getArticles('http://www.npr.org/rss/rss.php?id=1006', '#top');
