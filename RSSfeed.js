3c71051093067245c022cb824654283ae822e3ce
ChloeNoelle <curbatsch@student.neumont.edu>
2015-12-14 22:34:23 -0700

    Wow I did a lot

--- RSSfeed.js
+++ RSSfeed.js
@@ -3,7 +3,42 @@ $(document).ready(function () {
     getArticles('http://www.npr.org/rss/rss.php?id=1006', '#top');
     getArticles('http://www.npr.org/rss/rss.php?id=1004', '#wor');
     getArticles('http://world.kbs.co.kr/rss/rss_enternews.htm?lang=e', '#kpo');
+
+    setInterval(ticker, 5000);
 });
+
+var stockNum = 0;
+
+function ticker() {
+    var stockNames = [
+        'goog', 'msft', 'aapl', 'ssnlf', 'hmb', 'lnvgy', 'nflx', 'amgn', 'alk', 'kr'
+    ];
+
+    if (stockNum === 9) {
+        stockNum = 0;
+    } else {
+        stockNum++;
+    }
+
+    getStocks(stockNames[stockNum]);
+}
+
+//pass in the urls seperately-
+function getStocks(stockUrl) {
+    $.ajax({
+        url: "http://www.google.com/finance/info?q=" + stockUrl,
+        dataType: "jsonp",
+        success: function (data) {
+            $.each(data, function (i, e) {
+                var stockString = e.t +
+                    "\nChange: " + e.c +
+                    "\nCurrent: " + e.l_cur;
+
+                $('#sto').html('<p>' + stockString + '</p>');
+            });
+        }
+    });
+}
 
 function getArticles(urltoarticles, articlesection) {
     $.ajax({
@@ -13,7 +48,7 @@ function getArticles(urltoarticles, articlesection) {
             if (data.responseData.feed && data.responseData.feed.entries) {
                 $.each(data.responseData.feed.entries.slice(0, 3), function (i, e) {
                     $(function () {
-                        $('<h3>' + e.title + '</h3>').appendTo(articlesection);
+                        $('<a href="' + e.link + '"><h3>' + e.title + '</h3></href>').appendTo(articlesection);
                         $('<p>' + e.content + '</p>').appendTo(articlesection);
                     });
                 });
